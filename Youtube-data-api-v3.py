f21be4ec2de3111fd7bf86f399a131be58d696ca
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-10-25 20:14:54 -0400

    Update Youtube-data-api-v3.py

--- Youtube-data-api-v3.py
+++ Youtube-data-api-v3.py
@@ -17,7 +17,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 # Replace 'your_keyword' with the keyword you want to search
 
-max_results = 200
+max_results = 1000
 serach_keyword = 'Desk setup 2023'
 
 video_id = []
@@ -91,48 +91,16 @@ df['transcript'] = get_transcripts(df['video_id'])
 df = df.dropna(subset=['transcript'])
 print(df.shape)
 
+# remove depulicate video id
+df['video_id'].drop_duplicates(inplace=True)
+
+print(df.shape)
+
 # get transcript text
-#df['transcript_text'] = df['transcript'].apply(lambda x: ' '.join([item['text'] for item in x]))
+df['transcript_text'] = df['transcript'].apply(lambda x: ' '.join([item['text'] for item in x]))
 # drop transcript column
-#df = df.drop('transcript', axis=1)
+df = df.drop('transcript', axis=1)
 # pass transcript text to openai api and get desk items from transcript
 
 # save df to csv first
 df.to_csv('youtube-desk-setup-raw-data.csv', index=False)
-
-def openai_api(text:str) -> str:
-    # openai api
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {
-                "role": "system",
-                "content": """You will be provided with Desk setup youtube video transcripts, and your task is to extract what items are mentioned in video: 
-                1. you must only extract items mentioned in the video 
-                2. you must find 3 main items in the video, computer, mouse and keyboard, if its not found in video, say \"NA\" 
-                3. if other desk items is mentioned also put them in the output, monitor, lights, desk, charger, computer dock etc.
-                4. if same category have multiple items put them in a string with comma separated.
-                5. your output format should be in json\n\n
-                here is one example of respond:
-                ```
-                {"computer": "14 inch MacBook Pro", "mouse": "Logitech MX Master 3s", "keyboard": "Logitech MX mechanical mini", "monitor": "Apple Studio display, BenQ PD 3220u", "lights": "Elgato key light air", "desk": "Ikea countertop, Alex drawers, Carly kitchen countertop", "charger": "CalDigit TS4 Thunderbolt Hub", "computer dock": "Book Arc by 12 South", "neon sign": "custom neon sign by Illusion Neon", "acoustic panels": "gig Acoustics panels", "desk chair": "Autonomous Ergo chair plus", "scanner": "Fujitsu scanner", "charging stand": "Pataka charging stand", "pen": "Grovemade pen", "sticky notes": "sticky notes", "webcam": "Opal C1", "microphone": "Shure MV7", "audio interface": "Apollo twin X", "speakers": "Yamaha HS5", "headphones": "Rode NTH100s", "mic arm": "Rode PSA1 Plus", "controller": "Tour Box Elite", "light control": "Elgato Stream Deck Plus", "tablet": "iPad Pro", "tablet arm": "Cooks you desk arm", "monitor mount": "BenQ monitor mount", "travel charger": "ESR travel charger", "desk mat": "Grovemade felt mat", "smart home device": "Amazon Alexa Show", "security cameras": "UV security cameras", "Mac Mini": "Mac Mini Pro"}
-                ```
-            """
-            },
-            {
-                "role":"user",
-                "content": text
-            }
-        ],
-        temperature=0,
-        max_tokens=1024,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    return response.choices[0]['message']['content']
-
-#df['items'] = df['transcript_text'].apply(openai_api)
-
-# save results to csv
-#df.to_csv('youtube-desk-setup.csv', index=False)
